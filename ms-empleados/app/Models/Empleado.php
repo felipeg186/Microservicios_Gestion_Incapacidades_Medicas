47cebdffd0e48f5cd59728a185a2d68f4cdee41c
felipeg186 <ofgualteros@uniboyaca.edu.co>
2026-06-04 18:35:06 -0500

    Felipe: [Creacion del archivo modelo de cada microservicio]

---
+++ ms-empleados/app/Models/Empleado.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    protected $table = 'empleados';
+    protected $fillable = [
+        'nombres',
+        'apellidos',
+        'documento',
+        'correo',
+        'telefono',
+        'cargo',
+        'area',
+        'fecha_ingreso',
+        'estado'
+    ];
+}
