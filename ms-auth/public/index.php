16e8159f6ebd5d0fdb73ddacf59a5358ceaad323
felipeg186 <ofgualteros@uniboyaca.edu.co>
2026-06-07 19:57:19 -0500

    Felipe: [creacion de los cors para conexion de Frontend y Backend; y se añadio la ruta de los cors en cada index de cada microservicio]

--- ms-auth/public/index.php
+++ ms-auth/public/index.php
@@ -4,6 +4,7 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/Config/Database.php';
 
+$cors = require __DIR__ . '/../app/Presentation/Middleware/CorsMiddleware.php';
 $routes = require __DIR__ . '/../app/Routes/auth.routes.php';
 
 $app = AppFactory::create();
